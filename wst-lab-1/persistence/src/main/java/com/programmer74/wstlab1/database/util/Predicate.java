f3f69fd817d5b387a9ebbc9f82f2ad091894bd26
Nikita Tomilov <programmer174@icloud.com>
2020-03-10 19:22:38 +0300

    Initial commit with somehow working lab1

---
+++ wst-lab-1/persistence/src/main/java/com/programmer74/wstlab1/database/util/Predicate.java
@@ -0,0 +1,22 @@
+package com.programmer74.wstlab1.database.util;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class Predicate {
+  StringBuilder builder;
+
+  public Predicate and(String where) {
+    if (builder == null) {
+      builder = new StringBuilder(where);
+    } else {
+      builder.append(" AND ").append(where);
+    }
+    return this;
+  }
+
+  @Override
+  public String toString() {
+    return builder.toString();
+  }
+}
